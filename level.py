e30f493ede4499ac0695d548efb154babc7fbab2
LeeDevs <yaidan120@gmail.com>
2022-01-30 01:17:48 -0500

    Starter RPG Setup

---
+++ level.py
@@ -0,0 +1,81 @@
+import pygame
+
+from settings import *
+
+from tile import Tile
+from player import Player
+
+from debug import debug
+
+
+class Level:
+    def __init__(self) -> None:
+
+        #get the display surface
+        self.dispaly_surface = pygame.display.get_surface()
+        
+        #Sprite Groups
+
+        self.visible_sprites = YSortCameraGroup()
+        self.obstacle_sprites = pygame.sprite.Group()
+
+        # sprite setup
+
+        self.create_map()
+
+    def create_map(self):
+        # for row_index, row in enumerate(WORLD_MAP):
+        #     for col_index, col in enumerate(row):
+
+        #         x = col_index * TILESIZE
+        #         y = row_index * TILESIZE
+
+        #         if col == 'x':
+        #             Tile((x ,y), [self.visible_sprites, self.obstacle_sprites])
+        #         if col == 'p':
+
+        
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
+
+                
+
+    def run(self):
+
+        #update and draw game
+        self.visible_sprites.custom_draw(self.player)
+        self.visible_sprites.update()
+        
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.dispaly_surface = pygame.display.get_surface()
+
+        self.half_width = self.dispaly_surface.get_size()[0] // 2
+        self.half_height = self.dispaly_surface.get_size()[1] // 2
+
+        self.offset = pygame.math.Vector2()
+
+        # Creating the floor
+
+        self.floor_surface = pygame.image.load('./graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surface.get_rect(topleft = (0, 0))
+
+    def custom_draw(self, player):
+
+        # Getting offset
+
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        # Drawing the floor
+
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.dispaly_surface.blit(self.floor_surface, floor_offset_pos)
+
+        #for sprite in self.sprites():
+        for sprite in sorted(self.sprites(), key= lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.dispaly_surface.blit(sprite.image, offset_pos)
